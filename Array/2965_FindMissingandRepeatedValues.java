c628e6256a60ca3412ef0267eaf7506f75222ec7
saison2005 <saisonavm1997@gmail.com>
2025-03-06 14:41:21 +0530

    first commit

---
+++ Array/2965_FindMissingandRepeatedValues.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        Set<Integer> set=new LinkedHashSet<>();
+        Set<Integer> dup=new LinkedHashSet<>();
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+              if(set.add(grid[i][j])){
+                dup.add(grid[i][j]))
+              }
+            }
+        }
+        List<Integer> list=new ArrayList<>(dup);
+       for(int i=1;i< )
+        
+    }
+}
