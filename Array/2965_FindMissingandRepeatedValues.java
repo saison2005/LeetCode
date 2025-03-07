e1dc278ba67cc346bc3d04c7778e376d139fd499
saison2005 <saisonavm1997@gmail.com>
2025-03-07 19:53:42 +0530

    add 2965

--- Array/2965_FindMissingandRepeatedValues.java
+++ Array/2965_FindMissingandRepeatedValues.java
@@ -5,13 +5,22 @@ class Solution {
 
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-              if(set.add(grid[i][j])){
-                dup.add(grid[i][j]))
+              if(!set.add(grid[i][j])){
+                dup.add(grid[i][j]);
               }
             }
         }
-        List<Integer> list=new ArrayList<>(dup);
-       for(int i=1;i<set.length)
-        
+       // List<Integer> list=new ArrayList<>(dup);
+       for(int i=1;i<=grid[0].length*grid.length;i++){
+        if(!set.contains(i)){
+            dup.add(i);
+            break;
+        }
+       }
+        int arr[]=new int[dup.size()],d=0;
+        for(int k:dup){
+            arr[d++]=k;
+        }
+        return arr;
     }
 }
